bd832f29bdbc4003796543a3300e1036754dcb15
lib001 <xilei.lee@gmail.com>
2022-09-06 17:58:48 +0800

    update migrate

--- tools/migrate.py
+++ tools/migrate.py
@@ -40,30 +40,42 @@ def resultdb_migrating(project, from_connection, to_connection):
 @click.option('--pool', default=10, help='cocurrent worker size.')
 @click.argument('from_connection', required=1)
 @click.argument('to_connection', required=1)
-def migrate(pool, from_connection, to_connection):
+@click.argument('project', default=None)
+def migrate(pool, from_connection, to_connection, project):
     """
     Migrate tool for pyspider
     """
     f = connect_database(from_connection)
     t = connect_database(to_connection)
 
-    if isinstance(f, ProjectDB):
-        for each in f.get_all():
-            each = unicode_obj(each)
-            logging.info("projectdb: %s", each['name'])
-            t.drop(each['name'])
-            t.insert(each['name'], each)
-    elif isinstance(f, TaskDB):
-        pool = Pool(pool)
-        pool.map(
-            lambda x, f=from_connection, t=to_connection: taskdb_migrating(x, f, t),
-            f.projects)
-    elif isinstance(f, ResultDB):
-        pool = Pool(pool)
-        pool.map(
-            lambda x, f=from_connection, t=to_connection: resultdb_migrating(x, f, t),
-            f.projects)
-
+    if not project:
+        if isinstance(f, ProjectDB):
+            for each in f.get_all():
+                each = unicode_obj(each)
+                logging.info("projectdb: %s", each['name'])
+                t.drop(each['name'])
+                t.insert(each['name'], each)
+        elif isinstance(f, TaskDB):
+            pool = Pool(pool)
+            pool.map(
+                lambda x, f=from_connection, t=to_connection: taskdb_migrating(x, f, t),
+                f.projects)
+        elif isinstance(f, ResultDB):
+            pool = Pool(pool)
+            pool.map(
+                lambda x, f=from_connection, t=to_connection: resultdb_migrating(x, f, t),
+                f.projects)
+    else:
+        if isinstance(f, TaskDB):
+            pool = Pool(pool)
+            pool.map(
+                lambda x, f=from_connection, t=to_connection: taskdb_migrating(x, f, t),
+                [project])
+        elif isinstance(f, ResultDB):
+            pool = Pool(pool)
+            pool.map(
+                lambda x, f=from_connection, t=to_connection: resultdb_migrating(x, f, t),
+                [project])
 
 if __name__ == '__main__':
     migrate()
