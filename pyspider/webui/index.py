bd89b614d15e52cbd9d8cb2de5eff60145acc01b
www <139439432@qq.com>
2023-08-03 14:01:58 +0800

    调整拷贝项目

--- pyspider/webui/index.py
+++ pyspider/webui/index.py
@@ -66,6 +66,7 @@ def get_feishu_app_token():
         "app_secret": "75LXpuQaXoWUtJZDTndynhBGcoZhtZMq"
     })
     headers = {
+        'Content-Type': 'application/json'
     }
     res = send_request(
         f'https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal?timestamp={int(time.time() * 1000)}',
