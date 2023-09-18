35425599a0ff9df128528bc25bb4b4842bc7fe21
www <139439432@qq.com>
2023-09-18 11:47:40 +0800

    调整获取飞书数据

--- pyspider/webui/index.py
+++ pyspider/webui/index.py
@@ -7,6 +7,8 @@
 
 import socket
 import hashlib
+import redis
+import requests
 import six
 from six import iteritems, itervalues
 from flask import render_template, request, json
@@ -31,6 +33,8 @@ config = configparser.ConfigParser()
 config.read('/opt/pyspider/key.config')
 app_id = config['FS']['app_id']
 app_secret = config['FS']['app_secret']
+
+redis_client = redis.StrictRedis(host='172.26.7.16', port=6379, db=5)
 
 
 def utf8(string):
@@ -65,9 +69,115 @@ def copy():
     return render_template("copy.html")
 
 
+# 获取user信息，outh2.0授权
+def get_user_info(code):
+    app_token = get_feishu_token(token_name='app_token')
+    if app_token is None:
+        return json.dumps({'err': '获取失败'})
+
+    url = "https://open.feishu.cn/open-apis/authen/v1/access_token"
+
+    payload = json.dumps({
+        "code": code,
+        "grant_type": "authorization_code"
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {app_token}',
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response.text
+
+
+# 获取飞书 app token 和 tenant token
+def get_feishu_token(token_name='token'):
+    old_token = redis_client.get(f'feishu:{token_name}')
+    if old_token is not None:
+        return old_token
+    payload = json.dumps({
+        "app_id": app_id,
+        "app_secret": app_secret,
+    })
+    headers = {
+        'Content-Type': 'application/json; charset=utf-8'
+    }
+    url = f"https://open.feishu.cn/open-apis/auth/v3/app_access_token/internal"
+    response = requests.request("post", url, headers=headers, data=payload)
+    # res = requests.request(method='post',
+    #                        url=f'https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal?timestamp={int(time.time() * 1000)}',
+    #                        headers=headers, data=payload)
+    token_info = response.json()
+    if token_info['code'] != 0:
+        return None
+    redis_client.set(f'feishu:token', token_info['tenant_access_token'])
+    redis_client.set(f'feishu:app_token', token_info['app_access_token'])
+    redis_client.expire(f'feishu:token', token_info['expire'] - 500)
+    return redis_client.get(f'feishu:{token_name}')
+
+
+# 获取飞书的任务
+def get_feishu_task():
+    # token和table是固定d的
+    token = 'bascn92h2DxjIZom4hsB1U9irLc'
+    table = 'tblBBtyYcEtpS7h2'
+    view_id = 'vewqaxH2xy'
+    filter_condi = 'filter=CurrentValue.%5B%E6%95%B0%E6%8D%AE%E6%8A%93%E5%8F%96%E7%8A%B6%E6%80%81%5D+%3D%22%E7%AD%89%E5%BE%85%E5%A4%84%E7%90%86%22'
+    url = f"https://open.feishu.cn/open-apis/bitable/v1/apps/{token}/tables/{table}/records?&page_size=200&view_id={view_id}"
+    payload = ''
+    u_token = get_feishu_token()
+    if u_token is None:
+        return {'err': 'token获取失败'}
+
+    headers = {
+        'Authorization': f'Bearer {u_token}',
+        'Content-Type': 'application/json',
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    data = response.json()
+    tmp_set = {}    # 所有的媒体
+    for item in data['data']['items']:
+        one = item['fields']
+        task = one['任务类型']
+        all_keys = item['fields'].keys()
+
+        if tmp_set.get(task) is None:
+            tmp_set[task] = []
+
+        for key in all_keys:
+            if key.startswith('关键词'):
+                tmp_set[task].append(one[key])
+    return tmp_set
+
+# 获取飞书抓取任务
+@app.route('/get_feishu_spider')
+def get_feishu_spider():
+    media = request.args.get('media', "")  # 数据渠道
+
+    task_info = get_feishu_task()
+
+    return json.dumps(task_info)
+
+
+# 飞书获取用户
+@app.route('/get_feishu_user')
+def get_feishu_user():
+    code = request.args.get('code', "")
+    if code == '':
+        return '{"err":"参数不能为空"}'
+    user_info = get_user_info(code)
+    return user_info
+
+
 # 飞书相关接口
 @app.route('/get_feishu_app_token')
 def get_feishu_app_token():
+    token = get_feishu_token()
+    return json.dumps({"token": token})
+
+
+# 飞书相关接口
+@app.route('/get_feishu_old_token')
+def get_feishu_old_token():
     data = json.dumps({
         "app_id": app_id,
         "app_secret": app_secret,
@@ -84,6 +194,7 @@ def get_feishu_app_token():
         return res.decode('utf-8')
     except Exception as e:
         return e
+
 
 @app.route('/get_feishu_excel')
 def get_feishu_excel():
